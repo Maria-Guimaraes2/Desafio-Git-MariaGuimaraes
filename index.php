b46186475f8c44872c55448b9c8cbabe55d92417
Maria-Guimaraes2 <maendesx@gmail.com>
2026-09-24 14:56:02 -0300

    Adiciona matricula e email do aluno

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 $nome = "Maria Eduarda Guimarães"; 
 $idade = 16;
 $curso = "Desenvolvimento de Sistemas";
+$matricula = "4055041";
+$email = "maendesx@gmail.com";
 
 echo "<h1> Cadastro de alunos</h1>";
 echo "Nome: " . $nome . "<br>"; 
+echo "Número de Matricula: " . $matricula . "<br>";
 echo "Idade: " . $idade . "<br>";
+echo "Email: " . $email . "<br>";
 echo "Seu curso: " . $curso . "<br>";
 
 ?>
